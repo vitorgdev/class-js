f93eb4327c9f3bdf7b85337c2e4103dd37d4b5d0
Mauro <mauromaio.dev@gmail.com>
2023-10-03 20:12:14 -0300

    feat: rotas de auth e user

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose, { ConnectOptions } from "mongoose";
+import authRoutes from "./routes/authRoutes";
+import userRoutes from "./routes/userRoutes";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/users", userRoutes);
+app.use("/auth", authRoutes);
 
 mongoose
   .connect(
